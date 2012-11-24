6f8128e8b34f06b27f4d153edc22f87c01672658
ThePaavero <nospam@astudios.org>
2012-11-25 01:03:25 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-percentify
+Percentify
 ==========
+
+##Helps you turn a pixel based CSS into percentage based CSS##
+
+##Usage Example##
+&lt;script type='text/javascript'&gt;
+var P = new Percentifier(false);
+&lt;/script&gt;
---
+++ percentify.js
@@ -0,0 +1,220 @@
+/**
+ * Percentify! Converts pixels to percentages for a more responsive layout.
+ * Either shows the percentages or actually modifies the CSS properties
+ *
+ * @package Percentify
+ * @uses jQuery
+ */
+var Percentifier = function(modify, custom_selectors) {
+
+	/*global $, console*/
+	'use strict';
+
+	var self              = this;
+	var default_selectors = 'div, aside, article, img, h1, h2, h3, h4, p, span, form, a';
+	var selectors         = custom_selectors || default_selectors;
+	var css_syntax        = '';
+	var createdSelectors  = {};
+
+	// Modify the CSS properties of the elements? If not, just show them
+	modify = modify || false;
+
+	// -----------------------------------------------------------------------
+
+	/**
+	 * Run this thing
+	 * @return void
+	 */
+	this.init = function()
+	{
+		traverse(selectors, modify);
+	};
+
+	// -----------------------------------------------------------------------
+
+	/**
+	 * Traverse elements
+	 * @param  {string} s jQuery selector string
+	 * @return void
+	 */
+	var traverse = function(s, modify)
+	{
+		$(s).each(function()
+		{
+			var percentages = doElement($(this));
+
+			if(modify === true)
+			{
+				modifyCSS($(this), percentages);
+			}
+			else
+			{
+				$(this).data('percentages', percentages);
+				showPercentages($(this), percentages);
+				createCSSsyntax($(this));
+			}
+		});
+
+		if(modify === false)
+		{
+			console.log('Percentify gives you CSS code to copy and paste:');
+			console.log(css_syntax);
+		}
+	};
+
+	/**
+	 * Calculate the percentages for an element
+	 * @param  {object} el HTML element
+	 * @return {object}
+	 */
+	var doElement = function(el)
+	{
+		var my_width          = $(el).width();
+		var my_height         = $(el).height();
+		var my_parent         = $(el).parent();
+		var my_padding_top    = parseInt($(el).css('padding-top'), 10);
+		var my_padding_bottom = parseInt($(el).css('padding-bottom'), 10);
+		var my_padding_left   = parseInt($(el).css('padding-left'), 10);
+		var my_padding_right  = parseInt($(el).css('padding-right'), 10);
+
+		var parent_width  = $(my_parent).width();
+		var parent_height = $(my_parent).height();
+
+		var percentages = {
+			width          : (my_width / parent_width) * 100,
+			height         : (my_height / parent_height) * 100,
+			padding_top    : (my_padding_top / parent_height) * 100,
+			padding_bottom : (my_padding_bottom / parent_height) * 100,
+			padding_left   : (my_padding_left / parent_width) * 100,
+			padding_right  : (my_padding_right / parent_width) * 100
+		};
+
+		return percentages;
+	};
+
+	/**
+	 * Do the visual stuff
+	 * @param  {object} el          HTML element
+	 * @param  {object} percentages
+	 * @return void
+	 */
+	var showPercentages = function(el, percentages)
+	{
+		var indicator = document.createElement('div');
+			indicator.className = 'percentages_indicator';
+			indicator.innerHTML = percentages.width + ' * ' + percentages.height;
+			indicator.innerHTML += '<br/>width: ' + percentages.width + '%;';
+
+		$(indicator).css({
+			'position'   : 'absolute',
+			'font-size'  : '10px',
+			'padding'    : '2px',
+			'background' : '#007ec2',
+			'color'      : '#fff',
+			'opacity'    : '.1'
+		});
+
+		$(indicator).mouseenter(function()
+		{
+			$(indicator).css({
+				'opacity' : '1'
+			});
+		});
+
+		$(indicator).mouseleave(function()
+		{
+			$(indicator).css({
+				'opacity' : '.1'
+			});
+		});
+
+		$(el).prepend(indicator);
+	};
+
+	/**
+	 * Modify the width of given element
+	 * @param  {object} el          HTML element
+	 * @param  {object} percentages
+	 * @return void
+	 */
+	var modifyCSS = function(el, percentages)
+	{
+		$(el).css({
+			'width' : percentages.width + '%'
+		});
+	};
+
+	/**
+	 * Create CSS styntax for given element
+	 * @param  {object} el HTML element
+	 * @return void
+	 */
+	var createCSSsyntax = function(el)
+	{
+		var p          = $(el).data('percentages');
+		var selector   = '';
+		var id         = $(el).attr('id');
+		var class_name = $(el).className;
+		var tag_type   = $(el).prop('tagName').toLowerCase();
+
+		if(id !== undefined)
+		{
+			selector = '#' + id;
+		}
+		else if(class_name !== undefined)
+		{
+			selector = '.' + class_name;
+		}
+		else if(tag_type !== undefined)
+		{
+			selector = tag_type;
+		}
+
+		if(createdSelectors[selector])
+		{
+			// Already done this selector, don't want duplicates
+			return false;
+		}
+
+		// Remember that we've already done this selector
+		createdSelectors[selector] = true;
+
+		css_syntax += selector + ' {\n';
+
+		css_syntax += createCSSproperty('width', p.width);
+		css_syntax += createCSSproperty('padding-top', p.padding_top);
+		css_syntax += createCSSproperty('padding-bottom', p.padding_bottom);
+		css_syntax += createCSSproperty('padding-left', p.padding_left);
+		css_syntax += createCSSproperty('padding-right', p.padding_right);
+
+		css_syntax += '}\n\n';
+	};
+
+	/**
+	 * Create a CSS property line
+	 * @param  {string} property i.e. 'padding-bottom'
+	 * @param  {integer} value   i.e. 20.30020
+	 * @return {string}          CSS syntax, i.e. 'padding-bottom: 10%;'
+	 */
+	var createCSSproperty = function(property, value)
+	{
+		var my_string = '';
+
+		value = parseInt(value, 10);
+
+		if(value > 0 && isFinite(value))
+		{
+			my_string =  '\t' + property + ': ' + value + '%;\n';
+		}
+
+		return my_string;
+	};
+
+	// -----------------------------------------------------------------------
+
+	// Auto-initiate
+	this.init();
+
+}; // End Percentifier
+
+// EOF
